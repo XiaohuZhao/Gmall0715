b908f33a08557c536c4ebe01216d33d39976a59d
12498 <1249859919@qq.com>
2018-12-08 08:37:59 +0800

    完成从购物车前往交易页面，合并cookie和数据库中勾选的商品

---
+++ gmall-passport-web/src/main/java/com/atguigu/gmall0715/passport/GmallPassportWebApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall0715.passport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.atguigu.gmall0715")
+public class GmallPassportWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallPassportWebApplication.class, args);
+    }
+}
---
+++ gmall-passport-web/src/test/java/com/atguigu/gmall0715/passport/GmallPassportWebApplicationTests.java
@@ -0,0 +1,33 @@
+package com.atguigu.gmall0715.passport;
+
+import com.atguigu.gmall0715.passport.util.JwtUtil;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallPassportWebApplicationTests {
+
+    @Test
+    public void test01(){
+        String key = "atguigu";
+        String ip="192.168.197.233";
+        Map map = new HashMap();
+        map.put("userId","1001");
+        map.put("nickName","marry");
+        String token = JwtUtil.encode(key, map, ip);
+        Map<String, Object> decode = JwtUtil.decode(token, key, "192.168.197.233");
+    }
+
+
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
